9d9d6b52ad45a3932c778641349fa6d2b68b000d
DavidPav3l <david_marian@outlook.com>
2023-05-25 12:59:27 +0300

    primul commit dat de pe webstorm

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,16 @@
-import '../styles/index.css';
-const img = document.querySelector('img');
-img.classList.add('rounded-xl');
-let yes = 10;
+import "../styles/index.css";
+const img = document.querySelector("img");
+img.classList.add("rounded-xl");
+let yes;
 yes = 5;
 console.log(yes);
 const GetCash = async function () {
-    console.log('cash');
+  console.log("cash");
 };
 
 const mama = async function () {
-    await GetCash();
+  await GetCash();
 };
-mama();
+mama().then(() => {
+  console.log("caca");
+});
